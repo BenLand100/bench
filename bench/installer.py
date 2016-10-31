a0a80805790e9ed60427b49a5e489d48f49dcd16
Benjamin Land <benland100@gmail.com>
2016-10-31 13:21:56 -0700

    Support older python and fix path errors.

--- bench/installer.py
+++ bench/installer.py
@@ -69,15 +69,18 @@ def install_rat_snapshot(install_directory, commit_hash, base_environment, token
     # Note, the created folder will [fork]-rat-[commit]
     # but the commit hash in the DB could be the short 8 char version.
     # Best to ensure we install to the same as the commit char...
-    with tarfile.open(zipname) as f:
-        temp_dir = os.path.join(install_directory, "_temp_")
+    f = tarfile.open(zipname)
+    temp_dir = os.path.join(install_directory, "_temp_")
+    try:    
         f.extractall(temp_dir)
-        # Now just move the installed software across
-        folders = glob.glob(os.path.join(temp_dir, '{0}-rat-*'.format(fork)))
-        if len(folders) != 1:
-            raise IndexError("Unexpected folder structure! {0}".format(folders))
-        shutil.move(folders[0], sw_path)
-
+    except:
+       raise Exception('Could not extract archive.')
+    
+    # Now just move the installed software across
+    folders = glob.glob(os.path.join(temp_dir, '{0}-rat-*'.format(fork)))
+    if len(folders) != 1:
+        raise IndexError("Unexpected folder structure! {0}".format(folders))
+    shutil.move(folders[0], sw_path)
     os.chmod(os.path.join(sw_path, 'configure'), stat.S_IRWXU)
     
     # Configure and install
@@ -88,7 +91,7 @@ def install_rat_snapshot(install_directory, commit_hash, base_environment, token
         for line in env_file.readlines():
             if not re.match(pattern, line):
                 base_text += '{0}\n'.format(line)
-                
+    
     # Write this text to file
     temp_env = os.path.join(install_directory, 'installerTemp.sh')
     temp_script = os.path.join(os.getcwd(), '_temp_.sh')
@@ -103,7 +106,7 @@ cd {dir}\n\
 ./configure\n\
 source env.sh\n\
 scons\n\
-exit $?'.format(env = env_path, dir = sw_path))
+exit $?'.format(env = temp_env, dir = sw_path))
     rtc = os.system('/bin/bash {0}'.format(temp_script))
     if rtc != 0:
         raise RuntimeError("Unable to install RAT")
